6a5ac347d8e3ca6e3ad64f4ac5bf58e20ff2ef13
Elise Frazier <efrazie7@uncc.edu>
2024-06-22 17:43:20 -0400

    Added Reserve a table button

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,27 +7,14 @@ function Main() {
     <main>
       <div className="hero">
         <div className="hero-details">
-          <p className="hero-title">Little Lemon 
-            <p className="hero-description">We are a family owned Mediterranean restaurant, focused on
-          traditional recipes served with a modern twist. </p> </p>
-          {/* <p
-            style={{
-              width: "100%",
-              flexBasis: "50%",
-              margin: "10px 0",
-              padding: "20px",
-              boxSizing: "border-box",
-              display: "flex",
-              alignItems: "center",
-              textAlign: "center",
-              justifyContent: "center",
-            }} className="hero-description"
-          >
-            {/* <p className="hero-description"> */}
-              {/* We are a family owned Mediterranean restaurant, focused on
-              traditional recipes served with a modern twist. */}
-            {/* </p> */}
-          {/* </p>  */}
+          <div className="hero-title">
+            Little Lemon
+            <p className="hero-description">
+              We are a family owned Mediterranean restaurant, focused on
+              traditional recipes served with a modern twist.
+            </p>
+            <button className="reserve-btn">Reserve a Table</button>
+          </div>
         </div>
       </div>
       <div className="section-container">
