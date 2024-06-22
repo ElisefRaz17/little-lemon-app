93824630643cea7bcf334c49ec8a52cc88d167d8
Elise Frazier <efrazie7@uncc.edu>
2024-06-22 18:51:10 -0400

    updating change to move hero image to correct position

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "./Cards/Cards";
+import headerPhoto from "../assets/restauranfood.jpg";
 import styles from "../styles/main.css";
 
 function Main() {
@@ -14,10 +15,37 @@ function Main() {
               traditional recipes served with a modern twist.
             </p>
             <div>
-            <button className="reserve-btn">Reserve a Table</button>
+              <button className="reserve-btn">Reserve a Table</button>
             </div>
+            {/* <div
+              style={{
+                width: "100%",
+                justifyContent: "flex-end",
+                display: "flex",
+              }}
+            > */}
+            <img
+              src={headerPhoto}
+              style={{
+                borderRadius: "16px",
+                width: "375px",
+                height: "387px",
+                justifyContent: "flex-end",
+                display: "flex",
+                position:'relative',
+                marginTop:'-200px',
+                marginLeft: 'auto'
+              }}
+              alt="hero"
+            />
+            {/* </div> */}
           </div>
+          {/* <div style={{float:'right', width:'49%'}}>
+            <img src={headerPhoto} style={{borderRadius:'16px', width:'375px', height:'387px'}}/>
+
+          </div> */}
         </div>
+        {/* <img src={headerPhoto} style={{borderRadius:'16px', width:'375px', height:'387px', float:'right', position:'relative', bottom:'230px', right:'400px'}}/> */}
       </div>
       <div className="section-container">
         <span className="section-title">This weeks specials!</span>
